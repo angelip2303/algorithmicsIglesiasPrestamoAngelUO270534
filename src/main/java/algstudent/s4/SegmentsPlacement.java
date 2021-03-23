a7ea1f6cd92715eacf7a5b5e279b9edcea475edb
angel <angel@DESKTOP-SGQP321>
2021-03-23 22:31:54 +0100

    Uploading the files for the submission

--- src/main/java/algstudent/s4/SegmentsPlacement.java
+++ src/main/java/algstudent/s4/SegmentsPlacement.java
@@ -2,20 +2,37 @@ package algstudent.s4;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Collections;
 
 public class SegmentsPlacement {
 	
 	private static final String FILE_NAME = "src/main/java/algstudent/s4/game1.txt";
 	
-	private int n = Integer.MAX_VALUE; // Number of segments.
+	private int n; // Number of segments.
 	private Integer[] l; // Length of each segment.
 	
-	private ArrayList<Pair> sortedPairs1 = new ArrayList<Pair>();
-	private ArrayList<Pair> sortedPairs2 = new ArrayList<Pair>();
-	private ArrayList<Pair> sortedPairs3 = new ArrayList<Pair>();
+	private LinkedList<Pair> sortedPairs1;
+	private LinkedList<Pair> sortedPairs2;
+	private LinkedList<Pair> sortedPairs3;
 	
+	public SegmentsPlacement(Integer[] l) {
+		this.n = l.length;
+		this.l = l;
+		
+		this.sortedPairs1 = new LinkedList<Pair>();
+		this.sortedPairs2 = new LinkedList<Pair>();
+		this.sortedPairs3 = new LinkedList<Pair>();
+	}
+	
+	public SegmentsPlacement() {
+		this.n = Integer.MAX_VALUE; // Number of segments.
+		
+		this.sortedPairs1 = new LinkedList<Pair>();
+		this.sortedPairs2 = new LinkedList<Pair>();
+		this.sortedPairs3 = new LinkedList<Pair>();
+	}
+
 	public static void main (String args []) {
 		SegmentsPlacement sp = new SegmentsPlacement();
 		
@@ -28,13 +45,13 @@ public class SegmentsPlacement {
 		
 		// IO --> through the console.
 		sp.print(sp.sortedPairs1);
-		System.out.printf("Cost of greedy 1 = %.2f pufosos\n\n", greedy1);
+		System.out.printf("Cost of greedy 1 = %.2f pufosos%n%n", greedy1);
 		
 		sp.print(sp.sortedPairs2);
-		System.out.printf("Cost of greedy 2 = %.2f pufosos\n\n", greedy2);
+		System.out.printf("Cost of greedy 2 = %.2f pufosos%n%n", greedy2);
 		
 		sp.print(sp.sortedPairs3);
-		System.out.printf("Cost of greedy 3 = %.2f pufosos\n\n", greedy3);
+		System.out.printf("Cost of greedy 3 = %.2f pufosos%n%n", greedy3);
 	}
 	
 	private void readFromFile(String fileName) {
@@ -62,45 +79,45 @@ public class SegmentsPlacement {
 		}
 	}
 	
-	private double greedy1() {
-		// We create a ArrayList of Pairs so that we store the value of the segment (number) and the length.
+	double greedy1() {
+		// We create a LinkedList of Pairs so that we store the value of the segment (number) and the length.
 		for (int i = 0; i < l.length; i ++) {
 			sortedPairs1.add(new Pair(i, l[i]));
 		}
 		
-		// We sort the ArrayList of Pairs.
+		// We sort the LinkedList of Pairs.
 		
 		// We perform the operations.
 		return calculatePufosos(sortedPairs1);
 	}
 	
-	private double greedy2() {
-		// We create a ArrayList of Pairs so that we store the value of the segment (number) and the length.
+	double greedy2() {
+		// We create a LinkedList of Pairs so that we store the value of the segment (number) and the length.
 		for (int i = 0; i < l.length; i ++) {
 			sortedPairs2.add(new Pair(i, l[i]));
 		}
 		
-		// We sort the ArrayList of Pairs.
+		// We sort the LinkedList of Pairs.
 		Collections.sort(sortedPairs2, Collections.reverseOrder()); // --> Sorted in descending order. 
 		
 		// We perform the operations.
 		return calculatePufosos(sortedPairs2);
 	}
 	
-	private double greedy3() {
-		// We create a ArrayList of Pairs so that we store the value of the segment (number) and the length.
+	double greedy3() {
+		// We create a LinkedList of Pairs so that we store the value of the segment (number) and the length.
 		for (int i = 0; i < l.length; i ++) {
 			sortedPairs3.add(new Pair(i, l[i]));
 		}
 		
-		// We sort the ArrayList of Pairs.
+		// We sort the LinkedList of Pairs.
 		Collections.sort(sortedPairs3); // --> Sorted in ascending order. 
 		
 		// We perform the operations.
 		return calculatePufosos(sortedPairs3);
 	}
 	
-	private double calculatePufosos(ArrayList<Pair> sortedPairs) {
+	private double calculatePufosos(LinkedList<Pair> sortedPairs) {
 		int x = 0;
 		int y = 0;
 		double mid = 0;
@@ -114,7 +131,7 @@ public class SegmentsPlacement {
 		return mid;
 	}
 	
-	private void print(ArrayList<Pair> sortedPairs) {
+	private void print(LinkedList<Pair> sortedPairs) {
 		int x = 0;
 		int y = 0;
 		int segmentPosition = 0;
@@ -125,7 +142,7 @@ public class SegmentsPlacement {
 			segmentPosition = value.segmentsPosition;
 			midPoint = ((double) (x + y)) / 2;
 			
-			System.out.printf("S%d: (%d to %d), midpoint = %.2f\n", segmentPosition, x, y, midPoint);
+			System.out.printf("S%d: (%d to %d), midpoint = %.2f%n", segmentPosition, x, y, midPoint);
 			
 			x = y;
 		}
@@ -142,7 +159,13 @@ public class SegmentsPlacement {
 
 		@Override
 		public int compareTo(Pair o) {
-			return this.length - o.length;
+			int ans = this.length - o.length;
+			
+			if(ans == 0) {
+				return this.segmentsPosition - o.segmentsPosition;
+			}
+			
+			return ans;
 		}
 	}
 	
--- src/main/java/algstudent/s4/SegmentsTimes.java
+++ src/main/java/algstudent/s4/SegmentsTimes.java
@@ -1,5 +1,64 @@
 package algstudent.s4;
 
+import java.util.Random;
+
 public class SegmentsTimes {
 
+	private static final int N_TIMES = 1000;
+	
+	public static void main(String args[]) {
+		for (int i = 100; i <= Integer.MAX_VALUE; i *= 2) {
+			// -*- GREEDY 1 -*-
+			
+	        SegmentsPlacement sp = new SegmentsPlacement(generateLengths(i));	
+	        long t1 = System.currentTimeMillis();
+	        
+	        for(int j = 0; j < N_TIMES; j++) {
+	        	sp.greedy1();
+	        }
+	        
+	        long t2 = System.currentTimeMillis();          
+	        System.out.printf("SIZE: %d  -  The time for the algorithm O(n) - greedy1 is: %d microseconds%n", i, (t2 - t1));
+	        	
+	        // -*- GREEDY 2 -*-
+	        
+	        sp = new SegmentsPlacement(generateLengths(i));	
+	        t1 = System.currentTimeMillis();
+	        
+	        for(int j = 0; j < N_TIMES; j++) {
+	        	sp.greedy2();
+	        }
+	        
+	        t2 = System.currentTimeMillis();          
+	        System.out.printf("SIZE: %d  -  The time for the algorithm O(n log(n)) - greedy2 is: %d microseconds%n", i, (t2 - t1));
+	        
+	        // -*- GREEDY 3 -*-
+	        
+	        sp = new SegmentsPlacement(generateLengths(i));
+	        t1 = System.currentTimeMillis();
+	        
+	        for(int j = 0; j < N_TIMES; j++) {
+	        	sp.greedy3();
+	        }
+	        
+	        t2 = System.currentTimeMillis();          
+	        System.out.printf("SIZE: %d  -  The time for the algorithm O(n log(n)) - greedy3 is: %d microseconds%n", i, (t2 - t1));
+	        
+	        // -*- NEW LINE -*-
+	        
+	        System.out.println("\n****************************\n");
+		}
+	}
+	
+	private static Integer[] generateLengths(int size) {
+		Integer[] aux = new Integer[size];
+		Random rd = new Random();
+		
+		for(int i = 0; i < size; i++) {
+			aux[i] = rd.nextInt(Integer.MAX_VALUE);
+		}
+		
+		return aux;
+	}
+	
 }
